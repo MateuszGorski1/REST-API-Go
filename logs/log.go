613d1044c1064eee47e31151b65a7ac6c06882d8
Gorski Mateusz <mateusz.gorski@nokia.com>
2022-07-12 09:39:26 +0000

    refactoring logs

--- logs/log.go
+++ logs/log.go
@@ -11,9 +11,10 @@ import (
 	"github.com/rs/zerolog/hlog"
 )
 
+// C acts as a list of http.Handler constructors
 var C alice.Chain
 
-//Enables user to see application logs
+// ServeLogs enables user to see application logs
 func ServeLogs() {
 	log := zerolog.New(os.Stdout).With().
 		Logger()
@@ -23,23 +24,25 @@ func ServeLogs() {
 	C = C.Append(hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
 		params := strings.Split(r.URL.Path, "/")
 		var param1, param2 string
-		if len(params) < 3 {
-			param2 = "null"
-			param1 = "null"
-		} else if len(params) < 4 {
-			param2 = "null"
+		if len(params) == 3 {
 			param1 = params[2]
-		} else {
+		} else if len(params) == 4 {
 			param1 = params[2]
 			param2 = params[3]
 		}
-		hlog.FromRequest(r).Info().
+		e := hlog.FromRequest(r).Info().
 			Str("method", r.Method).
 			Stringer("url", r.URL).
-			Str("host", r.Host).
-			Str("param 1", param1).
-			Str("param 2", param2).
-			Msg("")
+			Str("host", r.Host)
+
+		if len(param1) != 0 {
+			e = e.Str("param1", param1)
+		}
+		if len(param2) != 0 {
+			e = e.Str("param2", param2)
+		}
+
+		e.Send()
 	}))
 
 }
